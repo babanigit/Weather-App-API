accad4f153e3fba142e2360e08aff87727c57468
bablu <aniketpanchal07@gmail.com>
2024-02-29 00:22:36 +0530

    new commit

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+
+import React from 'react'
+import './assets/thunder.jpg';
+
+const main = () => {
+  return (
+    <div
+    Style = {{
+      backgroundImage:
+      'url("./assets/thunder.jpg")',
+   }}
+    className=' h-screen w-full '>
+
+    </div>
+  )
+}
+
+export default main
