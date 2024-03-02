9d4e33b398b249754ff2898c3fa8a142c45363f5
bablu <aniketpanchal07@gmail.com>
2024-03-02 14:55:54 +0530

    added background

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,8 @@ import sunnyRainny from "../src/Assets/png/sunny-rainny.png";
 import thunder from "../src/Assets/png/thunder.png";
 import windySunny from "../src/Assets/png/windy-sunny.png";
 
+import daysand from "../src/background/day_sandy.jpg";
+import daysnow from "../src/background/daysnow.jpg";
 interface WeatherData {
   coord: {
     lon: number;
@@ -36,6 +38,21 @@ interface WeatherData {
   wind: {
     speed: number;
   };
+}
+
+interface MyStyle {
+  header: {
+    backgroundImage: string;
+    height: string;
+    backgroundSize: string;
+    backgroundRepeat: string;
+  };
+  content: {
+    height: string;
+    width: string;
+    backgroundColor: string;
+  };
+  bgOpac: string;
 }
 
 function App() {
@@ -82,13 +99,15 @@ function App() {
   }
 
   let data: string = sun;
-  let BackGround: string = "bg-red-200";
+  let bg: string = daysand;
+  let BackGround: string = "bg-red-300";
+  let Opacity: string = "bg-opacity-0";
 
   try {
     switch (WeatherIcon) {
       case "50n":
         data = sunnyClound;
-        BackGround = "bg-yello-200";
+        BackGround = "bg-yellow-200";
         break;
 
       case "01n":
@@ -125,11 +144,14 @@ function App() {
 
       case "50d":
         data = sunnyClound;
-        BackGround = "bg-yello-200";
+        BackGround = "bg-yellow-500";
         break;
 
       case "01d":
         data = sunnyClound;
+        // BackGround = "bg-yellow-200";
+        // Opacity= "bg-opacity-50"
+        bg = daysnow;
         break;
 
       case "02d":
@@ -142,6 +164,8 @@ function App() {
 
       case "04d":
         data = windySunny;
+        bg = daysnow;
+
         break;
 
       case "09d":
@@ -167,64 +191,85 @@ function App() {
     console.error(error);
   }
 
+  const myStyle: MyStyle = {
+    header: {
+      // background: 'rgba(0, 0, 0, 0.5)',
+      backgroundImage: `url(${bg})`,
+      height: "100vh",
+      // marginTop: "-70px",
+      // fontSize: "50px",
+      backgroundSize: "cover",
+      backgroundRepeat: "no-repeat",
+    },
+    content: {
+      height: "100%",
+      width: "100%",
+      backgroundColor: "rgba(0, 0, 0, 0.5)",
+    },
+    bgOpac: "bg-opacity-30",
+  };
+
   return (
     <>
-      <div
-        // style={{background:(red)}}
-        className={
-          " grid grid-flow-row grid-rows-4 h-screen w-full  " + BackGround
-        }
-      >
-        <div className="grid grid-flow-row  grid-rows-3 ">
-          <div className="grid  h-full row-span-2 place-items-center text-4xl ">
-            weather app
-          </div>
-          <div className=" grid grid-flow-col bg-yellow-300 h-full place-content-center  gap-2">
-            <input
-              className="h-12 w-full p-3 rounded-md "
-              placeholder="enter city/name..."
-              type="text"
-              value={search}
-              onChange={(e) => setSearch(e.target.value)}
-            />
-            <button
-              className="w-20 h-12 p-4 border-2 rounded-md place-content-center grid border-black "
-              onClick={searchPressed}
-            >
-              search
-            </button>
-          </div>
-        </div>
-
-        <div className=" grid row-span-3 grid-rows-8 grid-cols-4 gap-2 p-2 bg-blue-400 h-full w-full place-content-center">
-          <p className=" h-full w-full grid col-span-4 row-span-3 items-center justify-center flex-row border-2 rounded-lg text-2xl ">
-            {/* <div className=" w-12">{pngData}</div> */}
-            <div className=" w-32 grid place-items-center">
-              <img src={data} alt="" />
+      <div style={myStyle.header}>
+        <div
+          // style={myStyle.content}
+          className={
+            " grid grid-flow-row grid-rows-4 h-screen w-full bg-black " +
+            myStyle.bgOpac
+          }
+        >
+          <div className="grid grid-flow-row  grid-rows-3 ">
+            <div className="grid  h-full row-span-2 place-items-center text-4xl ">
+              weather app
             </div>
-          </p>
-          <p className="h-full w-full grid col-span-4 items-center justify-center flex-row bg border-2 rounded-lg text-2xl ">
-            city-{weatherName}
-          </p>
-          <p className="h-full w-full grid  items-center justify-center flex-row border-2 rounded-lg text-2xl ">
-            temperature- {weatherMainTemp}
-          </p>
-          <p className="h-full w-full grid  items-center justify-center flex-row border-2 rounded-lg text-2xl ">
-            weather- {weatherMain}
-          </p>
-          <p className="h-full w-full grid  col-span-2 items-center justify-center flex-row border-2 rounded-lg text-2xl  ">
-            description- {weatherDesc}
-          </p>
+            <div className=" grid grid-flow-col  h-full place-content-center  gap-2">
+              <input
+                className="h-12 w-full p-3 rounded-md "
+                placeholder="enter city/name..."
+                type="text"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+              />
+              <button
+                className="w-20 h-12 p-4 border-2 rounded-md place-content-center grid border-black "
+                onClick={searchPressed}
+              >
+                search
+              </button>
+            </div>
+          </div>
 
-          <p className="h-full w-full grid  items-center justify-center flex-row border-2 rounded-lg text-2xl ">
-            timezone- {timeZone}
-          </p>
-          <p className="h-full w-full flex items-center justify-center flex-row border-2 rounded-lg text-2xl ">
-            icon- {WeatherIcon}
-          </p>
-          <p className="h-full w-full flex items-center justify-center flex-row border-2 rounded-lg text-2xl ">
-            lon- {corLong}
-          </p>
+          <div className=" grid row-span-3 grid-rows-8 md:grid-rows-5 md:grid-cols-1 grid-cols-4 gap-2 p-2 h-full w-full place-content-center">
+            <p className=" h-full w-full grid col-span-4 row-span-3 items-center justify-center flex-row border-2 rounded-lg text-2xl ">
+              {/* <div className=" w-12">{pngData}</div> */}
+              <div className=" w-32 grid place-items-center">
+                <img src={data} alt="" />
+              </div>
+            </p>
+            <p className="h-full w-full grid col-span-4 items-center justify-center flex-row bg border-2 rounded-lg text-2xl ">
+              city-{weatherName}
+            </p>
+            <p className="h-full w-full grid md:col-span-4  items-center justify-center flex-row border-2 rounded-lg text-2xl ">
+              temperature- {weatherMainTemp}
+            </p>
+            <p className="h-full w-full grid md:col-span-4   items-center justify-center flex-row border-2 rounded-lg text-2xl ">
+              weather- {weatherMain}
+            </p>
+            <p className="h-full w-full grid md:col-span-4   col-span-2 items-center justify-center flex-row border-2 rounded-lg text-2xl  ">
+              description- {weatherDesc}
+            </p>
+
+            <p className="h-full w-full md:hidden grid  items-center justify-center flex-row border-2 rounded-lg text-2xl ">
+              timezone- {timeZone}
+            </p>
+            <p className="h-full w-full md:hidden flex items-center justify-center flex-row border-2 rounded-lg text-2xl ">
+              icon- {WeatherIcon}
+            </p>
+            <p className="h-full w-full md:hidden flex items-center justify-center flex-row border-2 rounded-lg text-2xl ">
+              lon- {corLong}
+            </p>
+          </div>
         </div>
       </div>
     </>
